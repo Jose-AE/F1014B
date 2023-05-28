c4627dfed69cce22fc5ff198dcd71ac1c2d472af
Gatuno7000 <107892696+Jose-AE@users.noreply.github.com>
2023-05-28 15:22:32 -0600

    T1 1.0 DONE

--- test.py
+++
@@ -1,9 +0,0 @@
-data = None
-
-while data != None:
-    try:
-        exel = input("Nombre del archivo de exel")
-        data = pd.read_excel(exel)
-        break
-    except:
-        print("error")
